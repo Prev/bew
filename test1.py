259a2bf96abf98774c3c5faea10be56281e268ba
youngsoo lee <prevdev@gmail.com>
2016-08-12 14:56:46 +0900

    update1

--- test1.py
+++ test1.py
@@ -1,27 +1,55 @@
 import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.svm import LinearSVC
+from sklearn.grid_search import GridSearchCV
 
-train_df = pd.read_pickle("soma_goods_train.df")
+
+train_df = pd.read_pickle("data/soma_goods_train.df")
 
 
-i = 0
+vectorizer = CountVectorizer()
 
-categories = []
+
+def get_cate(each) :
+	return ";".join([each[1]['cate1'],each[1]['cate2'],each[1]['cate3']])
+
+
+d_list = []
+cate_list = []
 
 for each in train_df.iterrows() :
-	# if i > 10 :	
-	# 	break
-
-	# print(each[1]['name'])
+	# if i > 1000: break
 	# i += 1
 
-	cat = each[1]['cate1'] + '\t' + each[1]['cate2'] + '\t\t' + each[1]['cate3']
+	cate = get_cate(each)
+	# print(cate)
 
-	if cat not in categories :
-		categories.append(cat)
+	d_list.append(each[1]['name'])
+	cate_list.append(cate)
 
-#print( train_df )
 
-categories.sort()
+cate_dict = dict(zip(list(set(cate_list)),range(len(set(cate_list)))))
 
-for cat in categories :
-	print( cat )
+
+y_list = []
+for each in train_df.iterrows() :
+	cate = get_cate(each)
+	y_list.append(cate_dict[cate])
+
+
+x_list = vectorizer.fit_transform(d_list)
+
+print('x,y list extract completed')
+
+
+# i = 0
+# for y in y_list :
+# 	print(str(cate_list[i]) + ' / ' + str(y))
+# 	i += 1
+
+
+svc_param = {'C':np.logspace(-2,0,20)}
+gs_svc = GridSearchCV(LinearSVC(), svc_param, cv=5, n_jobs=4)
+gs_svc.fit(x_list, y_list)
+
